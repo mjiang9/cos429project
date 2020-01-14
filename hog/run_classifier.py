7dfb8190e9fa1995971c3adc1077bbea78c14b21
Claire <clairedu777@gmail.com>
2020-01-14 11:28:23 -0500

    hog final

--- hog/run_classifier.py
+++ hog/run_classifier.py
@@ -1,3 +1,3 @@
 from test_face_classifier import test_face_classifier_l
 
-test_face_classifier_l(4020, 300, 9, 'train_50', 'training_nonfaces')
+test_face_classifier_l(300, 300, 9, 'train_50', 'training_nonfaces')
